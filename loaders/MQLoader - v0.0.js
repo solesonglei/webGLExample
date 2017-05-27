28504f0bfe18c52003a4a36e25a8a5bce08dbae1
Song Lei <solesonglei@hotmail.com>
2017-05-27 20:13:57 +0800

    ---- script_dragon.js 1. Add a framework for import an .obj file with texture coordinates info 2. Add a simple render framwork for testing vertex and fragment shader. ---- script.js A test example for better understanding of stencil test.
    ---- resources
    the resource files such as texture images refered by examples.

---
+++ loaders/MQLoader - v0.0.js
@@ -0,0 +1,340 @@
+
+THREE.MQLoader = function(manager){
+	this.manager = ( manager !== undefined ) ? manager : THREE.DefaultLoadingManager;
+}
+
+THREE.MQLoader.prototype = {
+	constructor: THREE.MQLoader,
+	
+	load: function(url, onLoad, onProgress, onError){
+		
+		 var scope  = this;
+		 
+		 var loader = new THREE.XHRLoader(scope.manager);
+		 loader.setResponseType('arraybuffer');
+		 loader.load(url, function(data){
+		 
+			 onLoad(scope.parse(data));
+						 
+		 }, onProgress, onError);
+	},
+	
+	parse: function(data)
+	{		
+		console.time( 'OBJLoader' );
+	
+		var reader = new DataView(data);
+		var cursor = 0;
+		var pid =  reader.getInt32(0,true);//set true as 32bit is stored in little-edian format by us.
+		cursor += 4;
+					
+		var modelNum = reader.getInt32(cursor, true);
+		cursor += 4;
+					
+		//modelNum = 1;
+		//console.log(modelNum);
+		var container = new THREE.Group();
+		for (var i = 0 ; i < modelNum; ++i)
+		{
+			var geometry = new THREE.BufferGeometry();
+			var all_vertices, normals;
+			var vertices;
+		
+			var modelNameLen = reader.getInt32(cursor, true);
+			cursor += 4;
+						
+			var modelName = '';
+			for (var j = 0; j < modelNameLen; ++j){
+				var character = reader.getInt8(cursor,true);
+				character =  String.fromCharCode(character);
+				cursor += 1;
+				modelName += character;
+			}
+						
+			var verticesNum = reader.getInt32(cursor,true);
+			cursor += 4;
+			var facesNum = reader.getInt32(cursor, true);
+			cursor += 4;
+			
+			all_vertices = new Float32Array(facesNum * 3 * 3);
+			vertices     = new Float32Array(verticesNum * 3);
+			colors       = new Uint8Array(facesNum * 3 * 3 );
+			
+			// Read Vertices
+			for (var j = 0 ; j < verticesNum ; ++j){
+				var x,y,z;
+				x = reader.getFloat32(cursor, true);
+				cursor += 4;
+				y = reader.getFloat32(cursor, true);
+				cursor += 4;
+				z = reader.getFloat32(cursor, true);
+				cursor += 4;
+				vertices[j * 3]     = x;
+				vertices[j * 3 + 1] = y;
+				vertices[j * 3 + 2] = z;
+			}
+			// Read Faces
+			for (var j = 0; j < facesNum;  ++j){
+			    var a,b,c; // indices of one face's vertices
+				a = reader.getInt32(cursor, true) - 1;
+				cursor += 4;
+				b = reader.getInt32(cursor, true) - 1;
+				cursor += 4;
+				c = reader.getInt32(cursor,true) - 1;
+				cursor += 4;
+				
+				all_vertices[j * 9]     = vertices[a * 3];
+				all_vertices[j * 9 + 1] = vertices[a * 3 + 1];
+				all_vertices[j * 9 + 2] = vertices[a * 3 + 2];
+				
+				all_vertices[j * 9 + 3] = vertices[b * 3];
+				all_vertices[j * 9 + 4] = vertices[b * 3 + 1];
+				all_vertices[j * 9 + 5] = vertices[b * 3 + 2];
+				
+				all_vertices[j * 9 + 6] = vertices[c * 3];
+				all_vertices[j * 9 + 7] = vertices[c * 3 + 1];
+				all_vertices[j * 9 + 8] = vertices[c * 3 + 2];
+			}
+			
+			var tooth_r = 225,
+				tooth_g = 219,
+				tooth_b = 201;
+			var gum_r = 193,
+				gum_g = 143,
+				gum_b = 139;
+				
+			for(var j = 0; j < facesNum * 3 * 3; j += 3){
+				if(modelName[modelNameLen - 1] == '0'){
+					///console.log()
+					colors[j]     = gum_r;
+					colors[j + 1] = gum_g;
+					colors[j + 2] = gum_b;			
+				}
+				else{
+					colors[j]     = tooth_r;
+					colors[j + 1] = tooth_g;
+					colors[j + 2] = tooth_b;					
+				}
+			}			
+			//console.log(modelName, all_vertices.length);
+			geometry.addAttribute( 'position', new THREE.BufferAttribute( all_vertices, 3 ) );
+			geometry.addAttribute( 'color',    new THREE.BufferAttribute( colors, 3, true ) );
+			var mesh = new THREE.Mesh(geometry);
+			mesh.name = modelName;
+			container.add(mesh);
+		}	
+		console.timeEnd( 'OBJLoader' );
+		return container;
+	}
+}
+
+// in case that some browser dont support DataView.
+if ( typeof DataView === 'undefined' ) {
+
+	DataView = function( buffer, byteOffset, byteLength ) {
+
+		this.buffer = buffer;
+		this.byteOffset = byteOffset || 0;
+		this.byteLength = byteLength || buffer.byteLength || buffer.length;
+		this._isString = typeof buffer === "string";
+
+	};
+
+	DataView.prototype = {
+
+		_getCharCodes: function( buffer, start, length ) {
+
+			start = start || 0;
+			length = length || buffer.length;
+			var end = start + length;
+			var codes = [];
+			for ( var i = start; i < end; i ++ ) {
+
+				codes.push( buffer.charCodeAt( i ) & 0xff );
+
+			}
+			return codes;
+
+		},
+
+		_getBytes: function ( length, byteOffset, littleEndian ) {
+
+			var result;
+
+			// Handle the lack of endianness
+			if ( littleEndian === undefined ) {
+
+				littleEndian = this._littleEndian;
+
+			}
+
+			// Handle the lack of byteOffset
+			if ( byteOffset === undefined ) {
+
+				byteOffset = this.byteOffset;
+
+			} else {
+
+				byteOffset = this.byteOffset + byteOffset;
+
+			}
+
+			if ( length === undefined ) {
+
+				length = this.byteLength - byteOffset;
+
+			}
+
+			// Error Checking
+			if ( typeof byteOffset !== 'number' ) {
+
+				throw new TypeError( 'DataView byteOffset is not a number' );
+
+			}
+
+			if ( length < 0 || byteOffset + length > this.byteLength ) {
+
+				throw new Error( 'DataView length or (byteOffset+length) value is out of bounds' );
+
+			}
+
+			if ( this.isString ) {
+
+				result = this._getCharCodes( this.buffer, byteOffset, byteOffset + length );
+
+			} else {
+
+				result = this.buffer.slice( byteOffset, byteOffset + length );
+
+			}
+
+			if ( ! littleEndian && length > 1 ) {
+
+				if ( Array.isArray( result ) === false ) {
+
+					result = Array.prototype.slice.call( result );
+
+				}
+
+				result.reverse();
+
+			}
+
+			return result;
+
+		},
+
+		// Compatibility functions on a String Buffer
+
+		getFloat64: function ( byteOffset, littleEndian ) {
+
+			var b = this._getBytes( 8, byteOffset, littleEndian ),
+
+				sign = 1 - ( 2 * ( b[ 7 ] >> 7 ) ),
+				exponent = ( ( ( ( b[ 7 ] << 1 ) & 0xff ) << 3 ) | ( b[ 6 ] >> 4 ) ) - ( ( 1 << 10 ) - 1 ),
+
+			// Binary operators such as | and << operate on 32 bit values, using + and Math.pow(2) instead
+				mantissa = ( ( b[ 6 ] & 0x0f ) * Math.pow( 2, 48 ) ) + ( b[ 5 ] * Math.pow( 2, 40 ) ) + ( b[ 4 ] * Math.pow( 2, 32 ) ) +
+							( b[ 3 ] * Math.pow( 2, 24 ) ) + ( b[ 2 ] * Math.pow( 2, 16 ) ) + ( b[ 1 ] * Math.pow( 2, 8 ) ) + b[ 0 ];
+
+			if ( exponent === 1024 ) {
+
+				if ( mantissa !== 0 ) {
+
+					return NaN;
+
+				} else {
+
+					return sign * Infinity;
+
+				}
+
+			}
+
+			if ( exponent === - 1023 ) {
+
+				// Denormalized
+				return sign * mantissa * Math.pow( 2, - 1022 - 52 );
+
+			}
+
+			return sign * ( 1 + mantissa * Math.pow( 2, - 52 ) ) * Math.pow( 2, exponent );
+
+		},
+
+		getFloat32: function ( byteOffset, littleEndian ) {
+
+			var b = this._getBytes( 4, byteOffset, littleEndian ),
+
+				sign = 1 - ( 2 * ( b[ 3 ] >> 7 ) ),
+				exponent = ( ( ( b[ 3 ] << 1 ) & 0xff ) | ( b[ 2 ] >> 7 ) ) - 127,
+				mantissa = ( ( b[ 2 ] & 0x7f ) << 16 ) | ( b[ 1 ] << 8 ) | b[ 0 ];
+
+			if ( exponent === 128 ) {
+
+				if ( mantissa !== 0 ) {
+
+					return NaN;
+
+				} else {
+
+					return sign * Infinity;
+
+				}
+
+			}
+
+			if ( exponent === - 127 ) {
+
+				// Denormalized
+				return sign * mantissa * Math.pow( 2, - 126 - 23 );
+
+			}
+
+			return sign * ( 1 + mantissa * Math.pow( 2, - 23 ) ) * Math.pow( 2, exponent );
+
+		},
+
+		getInt32: function ( byteOffset, littleEndian ) {
+
+			var b = this._getBytes( 4, byteOffset, littleEndian );
+			return ( b[ 3 ] << 24 ) | ( b[ 2 ] << 16 ) | ( b[ 1 ] << 8 ) | b[ 0 ];
+
+		},
+
+		getUint32: function ( byteOffset, littleEndian ) {
+
+			return this.getInt32( byteOffset, littleEndian ) >>> 0;
+
+		},
+
+		getInt16: function ( byteOffset, littleEndian ) {
+
+			return ( this.getUint16( byteOffset, littleEndian ) << 16 ) >> 16;
+
+		},
+
+		getUint16: function ( byteOffset, littleEndian ) {
+
+			var b = this._getBytes( 2, byteOffset, littleEndian );
+			return ( b[ 1 ] << 8 ) | b[ 0 ];
+
+		},
+
+		getInt8: function ( byteOffset ) {
+
+			return ( this.getUint8( byteOffset ) << 24 ) >> 24;
+
+		},
+
+		getUint8: function ( byteOffset ) {
+
+			return this._getBytes( 1, byteOffset )[ 0 ];
+
+		}
+
+	 };
+
+}
+
+
---
+++ script.js
@@ -0,0 +1,177 @@
+var main = function(){
+	var CANVAS = document.getElementById("your_canvas");
+	  CANVAS.width  = window.innerWidth;
+	  CANVAS.height = window.innerHeight;	
+
+	var GL;
+	try{
+		GL = CANVAS.getContext("experimental-webgl", {antialias:true});
+		var EXT = GL.getExtension("OES_element_index_uint") ||
+		  GL.getExtension("MOZ_OES_element_index_uint") ||
+			GL.getExtension("WEBKIT_OES_element_index_uint");	
+		} catch (e) {
+			alert("You are not webgl compatible :(") ;
+			return false;
+		}
+		
+	var shader_vertex_source = "\n\
+			attribute vec3 position; 		\n\
+			uniform vec4 color;  			\n\
+											\n\
+			varying vec4 vColor;				\n\
+											\n\
+			void main(void) {   			\n\
+				gl_Position = vec4(position, 1.); //0. is the z, and 1 is w			\n\
+				vColor = vec4(1.0, 0.0, 0.0, 1.0);									\n\
+				vColor = color;									\n\
+			}";
+
+
+  var shader_fragment_source = "\n\
+		precision mediump float;			\n\
+		varying vec4 vColor;				\n\
+											\n\
+		void main(void) {					\n\
+		gl_FragColor = vColor;				\n\
+	}";
+	
+	 var get_shader = function(source, type, typeString) {
+			var shader = GL.createShader(type);
+			GL.shaderSource(shader, source);
+			GL.compileShader(shader);
+			if (!GL.getShaderParameter(shader, GL.COMPILE_STATUS)) {
+			  alert("ERROR IN "+typeString+ " SHADER : " + GL.getShaderInfoLog(shader));
+			  return false;
+			}
+			return shader;
+	};
+	
+	  var shader_vertex		=	get_shader(shader_vertex_source, GL.VERTEX_SHADER, "VERTEX");
+	  var shader_fragment	=	get_shader(shader_fragment_source, GL.FRAGMENT_SHADER, "FRAGMENT");
+
+	  var SHADER_PROGRAM = GL.createProgram();
+	  GL.attachShader(SHADER_PROGRAM, shader_vertex);
+	  GL.attachShader(SHADER_PROGRAM, shader_fragment);
+
+	  GL.linkProgram(SHADER_PROGRAM);
+	  
+	  var _position = GL.getAttribLocation(SHADER_PROGRAM, "position");
+      var _color    = GL.getUniformLocation(SHADER_PROGRAM, "color"); 
+	  GL.enableVertexAttribArray(_position);	
+		  
+	 var vVertices = [
+		-0.75, 0.25, 0.50, 	// Quad #0
+		-0.25, 0.25, 0.50,
+		-0.25, 0.75, 0.50,
+		-0.75, 0.75, 0.50,
+		 0.25, 0.25, 0.90,	// Quad #1
+		 0.75, 0.25, 0.90, 
+		 0.75, 0.75, 0.90,
+		 0.25, 0.75, 0.90,
+		-0.75, -0.75, 0.50, // Quad #2
+		-0.25, -0.75, 0.50,
+		-0.25, -0.25, 0.50,
+		-0.75, -0.25, 0.50,
+		 0.25, -0.75, 0.50, // Quad #3
+		 0.75, -0.75, 0.50,
+		 0.75, -0.25, 0.50,
+		 0.25, -0.25, 0.50,
+		-1.00, -1.00, 0.00, // Quad #4
+		 1.00, -1.00, 0.00,
+		 1.00,  1.00, 0.00,
+		-1.00,  1.00, 0.00
+	 ]; 
+	 
+	 const numTests = 4;
+	 
+	 var indices = [
+		[0, 1, 2, 0, 2, 3],
+		[4, 5, 6, 4, 6, 7],
+		[8, 9, 10, 8, 10, 11],
+		[12, 13, 14, 12, 14, 15],
+		[16,17,18,16, 18, 19]
+	 ]
+	 
+	 var colors = [
+		[1.0, 0.0, 0.0, 1.0],
+		[0.0, 1.0, 0.0, 1.0],
+		[0.0, 0.0, 1.0, 1.0],
+		[1.0, 1.0, 0.0, 0.0]
+	 ];
+	 
+	 VERTEX_DATA = GL.createBuffer();
+	 GL.bindBuffer(GL.ARRAY_BUFFER, VERTEX_DATA);
+	 GL.bufferData(GL.ARRAY_BUFFER, new Float32Array(vVertices), GL.STATIC_DRAW);
+	 GL.vertexAttribPointer(_position, 3, GL.FLOAT, false, 0, 0); 
+	 
+	 var INDEX_DATA_0 = GL.createBuffer();
+	 var INDEX_DATA_1 = GL.createBuffer();
+	 var INDEX_DATA_2 = GL.createBuffer();
+	 var INDEX_DATA_3 = GL.createBuffer();
+	 var INDEX_DATA_4 = GL.createBuffer();
+	 
+	 var numStencilBits;
+	 var stencilValues = [0x7, 0x0, 0x2, 0xff];
+	 GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);	 
+	 GL.useProgram(SHADER_PROGRAM);// all of the object in the scene are shared one shader program.
+	 
+	 var animate = function(){
+		GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT | GL.STENCIL_BUFFER_BIT);			 
+		 // Test 0
+		 GL.stencilFunc(GL.LESS, 0x3, 0x7);
+		 GL.stencilOp(GL.REPLACE, GL.DECR, GL.DECR);
+		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_0);		 		 
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[0]), GL.STATIC_DRAW);
+		 //GL.uniform4fv(_color, colors[0]);	
+		 GL.drawElements(GL.TRIANGLES, 6, GL.UNSIGNED_SHORT, 0);
+		 
+		 // Test 1
+		 GL.stencilFunc(GL.GREATER, 0x3, 0x3);
+		 GL.stencilOp(GL.KEEP, GL.DECR, GL.KEEP);
+		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_1);
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[1]), GL.STATIC_DRAW);
+			GL.uniform4fv(_color, colors[1]);			 
+		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
+		 
+		 // Test 2
+		 GL.stencilFunc(GL.EQUAL, 0x1, 0x3);
+		 GL.stencilOp(GL.KEEP, GL.INCR, GL.INCR);
+		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_2);
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[2]), GL.STATIC_DRAW);
+		// GL.uniform4fv(_color, colors[2]);			 
+		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
+
+		 // Test 3
+		 GL.stencilFunc(GL.EQUAL, 0x2, 0x1);
+		 GL.stencilOp(GL.INVERT, GL.KEEP, GL.KEEP);
+		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_3);
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[3]), GL.STATIC_DRAW);	
+		 //GL.uniform4fv(_color, colors[3]);			 
+		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);
+		 
+		 numStencilBits = GL.getParameter( GL.STENCIL_BITS);
+		 stencilValues[3] = ~(((1 << numStencilBits) - 1) & 0x1) & 0xff;
+		 GL.stencilMask(0x0);
+		 
+		for(let i = 0; i < numTests; ++i){
+			GL.stencilFunc(GL.EQUAL, stencilValues[i], 0xff);
+			//GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_4);
+		    //GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[4]), GL.STATIC_DRAW);
+			
+			GL.uniform4fv(_color, colors[i]);			
+			GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
+		 }		
+
+		GL.flush();
+
+		window.requestAnimationFrame(animate);		 
+	 }
+	animate();
+	 
+}
+
+	  
+	  
+	   
+	  
+	  
