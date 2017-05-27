ec54c228ade154529bb0b310429176111ef17cf9
Song Lei <solesonglei@hotmail.com>
2017-05-28 00:59:06 +0800

    stencil test example complete

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var main = function(){
 
 	var GL;
 	try{
-		GL = CANVAS.getContext("experimental-webgl", {antialias:true});
+		GL = CANVAS.getContext("experimental-webgl", {antialias:true, stencil: true});
 		var EXT = GL.getExtension("OES_element_index_uint") ||
 		  GL.getExtension("MOZ_OES_element_index_uint") ||
 			GL.getExtension("WEBKIT_OES_element_index_uint");	
@@ -75,11 +75,11 @@ var main = function(){
 		 0.25, -0.75, 0.50, // Quad #3
 		 0.75, -0.75, 0.50,
 		 0.75, -0.25, 0.50,
-		 0.25, -0.25, 0.50,
-		-1.00, -1.00, 0.00, // Quad #4
-		 1.00, -1.00, 0.00,
-		 1.00,  1.00, 0.00,
-		-1.00,  1.00, 0.00
+		 0.25, -0.25, 0.50,	 
+		-1.00, -1.00, 0.50, // Quad #4
+		 1.00, -1.00, 0.50,
+		 1.00,  1.00, 0.50,
+		-1.00,  1.00, 0.50
 	 ]; 
 	 
 	 const numTests = 4;
@@ -89,7 +89,8 @@ var main = function(){
 		[4, 5, 6, 4, 6, 7],
 		[8, 9, 10, 8, 10, 11],
 		[12, 13, 14, 12, 14, 15],
-		[16,17,18,16, 18, 19]
+		[16,17,18,16, 18, 19],
+		[20,21,22,20, 22, 23]
 	 ]
 	 
 	 var colors = [
@@ -98,6 +99,9 @@ var main = function(){
 		[0.0, 0.0, 1.0, 1.0],
 		[1.0, 1.0, 0.0, 0.0]
 	 ];
+	 
+	 GL.enable(GL.STENCIL_TEST);
+	 //GL.enable(GL.DEPTH_TEST);
 	 
 	 VERTEX_DATA = GL.createBuffer();
 	 GL.bindBuffer(GL.ARRAY_BUFFER, VERTEX_DATA);
@@ -111,52 +115,63 @@ var main = function(){
 	 var INDEX_DATA_4 = GL.createBuffer();
 	 
 	 var numStencilBits;
-	 var stencilValues = [0x7, 0x0, 0x2, 0xff];
+	 var stencilValues = [0x7, 0x2, 0x2, 0xff];
+	 //stencilValues = [0xff, 0xff, 0xff, 0xff];
 	 GL.viewport(0.0, 0.0, CANVAS.width, CANVAS.height);	 
 	 GL.useProgram(SHADER_PROGRAM);// all of the object in the scene are shared one shader program.
 	 
 	 var animate = function(){
-		GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT | GL.STENCIL_BUFFER_BIT);			 
+		 GL.clear(GL.COLOR_BUFFER_BIT | GL.DEPTH_BUFFER_BIT | GL.STENCIL_BUFFER_BIT);
+		 GL.stencilMask(0xff);	
+		 GL.colorMask(0, 0, 0, 0);	
+
+		 //assign stencil buffer to 0xff
+		 GL.stencilFunc(GL.ALWAYS, 0x1, 0xff);
+		 GL.stencilOp(GL.KEEP, GL.KEEP, GL.REPLACE);
+		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_4);
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[4]), GL.STATIC_DRAW);
+		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
+		 
 		 // Test 0
-		 GL.stencilFunc(GL.LESS, 0x3, 0x7);
+		 GL.stencilFunc(GL.LESS, 0x7, 0x3); //  0x7
 		 GL.stencilOp(GL.REPLACE, GL.DECR, GL.DECR);
 		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_0);		 		 
 		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[0]), GL.STATIC_DRAW);
-		 //GL.uniform4fv(_color, colors[0]);	
 		 GL.drawElements(GL.TRIANGLES, 6, GL.UNSIGNED_SHORT, 0);
 		 
 		 // Test 1
-		 GL.stencilFunc(GL.GREATER, 0x3, 0x3);
+		 GL.stencilFunc(GL.GREATER, 0x3, 0x3); //0x3
 		 GL.stencilOp(GL.KEEP, GL.DECR, GL.KEEP);
 		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_1);
-		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[1]), GL.STATIC_DRAW);
-			GL.uniform4fv(_color, colors[1]);			 
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[1]), GL.STATIC_DRAW);		 
 		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
 		 
 		 // Test 2
 		 GL.stencilFunc(GL.EQUAL, 0x1, 0x3);
-		 GL.stencilOp(GL.KEEP, GL.INCR, GL.INCR);
+		 GL.stencilOp(GL.KEEP, GL.INCR, GL.INCR);// 0x4
 		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_2);
-		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[2]), GL.STATIC_DRAW);
-		// GL.uniform4fv(_color, colors[2]);			 
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[2]), GL.STATIC_DRAW);		 
 		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
 
 		 // Test 3
 		 GL.stencilFunc(GL.EQUAL, 0x2, 0x1);
-		 GL.stencilOp(GL.INVERT, GL.KEEP, GL.KEEP);
+		 GL.stencilOp(GL.INVERT, GL.KEEP, GL.KEEP);// 0x1
 		 GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_3);
-		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[3]), GL.STATIC_DRAW);	
-		 //GL.uniform4fv(_color, colors[3]);			 
-		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);
+		 GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[3]), GL.STATIC_DRAW);			 
+		 GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	 
 		 
 		 numStencilBits = GL.getParameter( GL.STENCIL_BITS);
+		 
+		// console.log(numStencilBits);
+		 
 		 stencilValues[3] = ~(((1 << numStencilBits) - 1) & 0x1) & 0xff;
 		 GL.stencilMask(0x0);
-		 
+		 GL.colorMask(1, 1, 1, 1);
+		 		 		 
 		for(let i = 0; i < numTests; ++i){
 			GL.stencilFunc(GL.EQUAL, stencilValues[i], 0xff);
-			//GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_4);
-		    //GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[4]), GL.STATIC_DRAW);
+			GL.bindBuffer(GL.ELEMENT_ARRAY_BUFFER, INDEX_DATA_4);
+		    GL.bufferData(GL.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices[4]), GL.STATIC_DRAW);
 			
 			GL.uniform4fv(_color, colors[i]);			
 			GL.drawElements(GL.TRIANGLES, 6,  GL.UNSIGNED_SHORT, 0);	
